1db63211cd592e3513341ab2f9eac12e6ef0bf8a
ledsna <45171833+ledsna@users.noreply.github.com>
2024-08-29 03:12:32 +0300

    outlines

--- Assets/Scripts/Outlines.cs
+++ Assets/Scripts/Outlines.cs
@@ -5,45 +5,25 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering;
 
-public class Outlines : ScriptableRendererFeature {
+public class ViewSpaceNormalsTexture : ScriptableRendererFeature {
 
     [System.Serializable]
-    public class OutlineSettings {
-        public float depthOutlineScale = 1.0f;
-        public float normalsOutlineScale = 1.0f;
+    public class TextureSettings {
 
-        [Header("Depth Settings")]
-        [Range(0.0f, 200.0f)]
-        public float depthThreshold = 1f;
-
-        [Header("Normal Settings")]
-        [Range(0.0f, 1.0f)]
-        public float normalsThreshold = 0.4f;
-
-        [Header("Normal Highlight Intensity")]
-        [Range(1.0f, 10f)]
-        public float highlightPower = 1.5f;
-
-        [Header("Depth Shadow Intensity")]
-        [Range(-1.0f, 5.0f)]
-        public float shadowPower = 0.5f;
-
-        [Header("General Scene View Space Normal Texture Settings")]
+        [Header("Texture Settings")]
         public RenderTextureFormat colorFormat;
         public int depthBufferBits;
         public FilterMode filterMode;
         public Color backgroundColor = Color.clear;
 
-        [Header("View Space Normal Texture Object Draw Settings")]
+        [Header("Texture Object Draw Settings")]
         public PerObjectData perObjectData;
         public bool enableDynamicBatching;
         public bool enableInstancing;
     }
 
-    private class OutlinePass : ScriptableRenderPass {
-        
-        private readonly Material OutlineMaterial;
-        private OutlineSettings settings;
+    private class NormalsTexturePass : ScriptableRenderPass {
+        private TextureSettings settings;
 
         private FilteringSettings filteringSettings;
 
@@ -55,17 +35,9 @@ public class Outlines : ScriptableRendererFeature {
 
         RTHandle temporaryBuffer;
 
-        public OutlinePass(RenderPassEvent renderPassEvent, LayerMask layerMask, OutlineSettings settings) {
+        public NormalsTexturePass(RenderPassEvent renderPassEvent, LayerMask layerMask, TextureSettings settings) {
             this.settings = settings;
             this.renderPassEvent = renderPassEvent;
-
-            OutlineMaterial = new Material(Shader.Find("Hidden/Outlines"));
-            OutlineMaterial.SetFloat("_DepthOutlineScale", settings.depthOutlineScale);
-            OutlineMaterial.SetFloat("_HighlightPower", settings.highlightPower);
-            OutlineMaterial.SetFloat("_ShadowPower", settings.shadowPower);
-            OutlineMaterial.SetFloat("_NormalsOutlineScale", settings.normalsOutlineScale);
-            OutlineMaterial.SetFloat("_DepthThreshold", settings.depthThreshold);
-            OutlineMaterial.SetFloat("_NormalsThreshold", settings.normalsThreshold);
 
             filteringSettings = new FilteringSettings(RenderQueueRange.opaque, layerMask);
 
@@ -95,7 +67,7 @@ public class Outlines : ScriptableRendererFeature {
         }
         
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
-            if (!OutlineMaterial || !normalsMaterial ||
+            if (!normalsMaterial ||
                 renderingData.cameraData.renderer.cameraColorTargetHandle.rt == null || temporaryBuffer.rt == null)
                 return;
 
@@ -116,18 +88,11 @@ public class Outlines : ScriptableRendererFeature {
             
             cmd.SetGlobalTexture(Shader.PropertyToID("_NormalsTexture"), normals.rt);
             
-            // using (new ProfilingScope(cmd, new ProfilingSampler("Outlines"))) {
-
-            //     Blitter.BlitCameraTexture(cmd, renderingData.cameraData.renderer.cameraColorTargetHandle, temporaryBuffer, OutlineMaterial, 0);
-            //     Blitter.BlitCameraTexture(cmd, temporaryBuffer, renderingData.cameraData.renderer.cameraColorTargetHandle);
-            // }
-
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
 
         public void Release(){
-            CoreUtils.Destroy(OutlineMaterial);
             CoreUtils.Destroy(normalsMaterial);
             normals?.Release();
             temporaryBuffer?.Release();
@@ -138,15 +103,15 @@ public class Outlines : ScriptableRendererFeature {
     [SerializeField] private RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingSkybox;
     [SerializeField] private LayerMask outlinesLayerMask;
     
-    [SerializeField] public OutlineSettings outlineSettings = new OutlineSettings();
+    [SerializeField] public TextureSettings outlineSettings = new TextureSettings();
 
-    private OutlinePass _outlinePass;
+    private NormalsTexturePass _outlinePass;
     
     public override void Create() {
         if (renderPassEvent < RenderPassEvent.BeforeRenderingPrePasses)
             renderPassEvent = RenderPassEvent.BeforeRenderingPrePasses;
 
-        _outlinePass = new OutlinePass(renderPassEvent, outlinesLayerMask, outlineSettings);
+        _outlinePass = new NormalsTexturePass(renderPassEvent, outlinesLayerMask, outlineSettings);
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
