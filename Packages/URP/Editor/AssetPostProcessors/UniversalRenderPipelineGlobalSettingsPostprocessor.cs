e2b24b308456f14ab3095fc54a4b2c474759f0a6
F1Soda <golik.tima@gmail.com>
2025-07-20 10:04:36 +0500

    Converted all files back to LF format

--- Packages/URP/Editor/AssetPostProcessors/UniversalRenderPipelineGlobalSettingsPostprocessor.cs
+++ Packages/URP/Editor/AssetPostProcessors/UniversalRenderPipelineGlobalSettingsPostprocessor.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using UnityEngine.Rendering.Universal;
 
 namespace UnityEditor.Rendering.Universal
--- Packages/URP/Editor/ShaderGraph/Targets/UniversalSixWaySubTarget.cs
+++ Packages/URP/Editor/ShaderGraph/Targets/UniversalSixWaySubTarget.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using UnityEngine;
 using UnityEditor.ShaderGraph;
 using UnityEngine.UIElements;
